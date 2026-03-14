b080b00a699c556083d90e47ed8ae49be976623f
Teddy <httpdevz@gmail.com>
2026-03-14 18:34:14 -0400

    fix: i'm an idiot

--- Casks/spartify.rb
+++ Casks/spartify.rb
@@ -1,11 +1,11 @@
 cask "spartify" do
-    version "1.0.0"
+    version "0.2.1"
     sha256 "REPLACE_WITH_SHA256_OF_DMG"
 
-    url "https://github.com/YOUR_USERNAME/spartify/releases/download/v#{version}/Spartify_#{version}_aarch64.dmg"
+    url "https://github.com/uhteddy/spartify/releases/download/v#{version}/Spartify_#{version}_aarch64.dmg"
     name "Spartify"
     desc "Spotify party hosting desktop app"
-    homepage "https://github.com/YOUR_USERNAME/spartify"
+    homepage "https://github.com/uhteddy/spartify"
 
     app "Spartify.app"
 end
